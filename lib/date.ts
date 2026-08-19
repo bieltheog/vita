982df698519876d5adcd898386dd9e613c286be5
bieltheog <bielopereira18@gmail.com>
2026-08-19 10:15:53 -0300

    util: padronizar data operacional no horário de Brasília

---
+++ lib/date.ts
@@ -0,0 +1,16 @@
+export function brazilDateKey(date = new Date()) {
+  return new Intl.DateTimeFormat("en-CA", {
+    timeZone: "America/Sao_Paulo",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
+}
+
+export function brazilDateTime(date = new Date()) {
+  return new Intl.DateTimeFormat("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    dateStyle: "short",
+    timeStyle: "short",
+  }).format(date);
+}
